0330f4f285276d3c8251c43696463760ecccb37e
Zaahid Vohra <67497624+zaahidvohra@users.noreply.github.com>
2025-12-18 00:11:05 +0530

    3dcarousal height fix

--- src/components/homeComponent/ImageCarousal3d.jsx
+++ src/components/homeComponent/ImageCarousal3d.jsx
@@ -16,14 +16,14 @@ export default function ImageCarousal3d() {
   return (
     // REMOVED 'ml-4' to fix centering
     // ADDED 'perspective-[1000px]' for better 3D depth perception
-<div className="h-96 md:h-[500px] lg:h-[600px] w-full flex items-center justify-center overflow-hidden bg-secondary perspective-[1000px]">      
+<div className="h-96 md:h-[500px] lg:h-[450px] w-full flex items-center justify-center overflow-hidden bg-secondary perspective-[1000px]">      
       {/* INCREASED SIZES:
           Mobile: w-[140px] h-[140px] (was 80px) -> Much bigger
           Sm: w-[160px] h-[160px]
           Md: w-[200px] h-[200px] 
           Lg: w-[240px] h-[240px]
       */}
-      <div className="relative lg:w-[240px] lg:h-[240px] md:w-[200px] md:h-[200px] sm:w-[160px] sm:h-[160px] w-[140px] h-[140px] [transform-style:preserve-3d] animate-[rotate_30s_linear_infinite]">
+      <div className="relative lg:w-[200px] lg:h-[200px] md:w-[200px] md:h-[200px] sm:w-[160px] sm:h-[160px] w-[140px] h-[140px] [transform-style:preserve-3d] animate-[rotate_30s_linear_infinite]">
         {images.map((src, index) => (
           <span key={index} style={{ "--i": index + 1 }} className="span-style">
             <img src={src} alt={`img - ${index + 1}`} className="img-style shadow-xl border-2 border-white/20" />
